ee5bd1e1b4922ee6d565f5eea0a4b42b1e344a68
Daniel Bonasser <danielbonasser@gmail.com>
2023-06-26 21:55:20 -0300

    Configure stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
-import { Inter } from 'next/font/google'
+import { styled } from '../styles/index';
 
-const inter = Inter({ subsets: ['latin'] })
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  },
+});
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Button>
+      <span>Test</span>
+      Enviar
+    </Button>
   )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import {createStitches} from '@stitches/react';
+
+export const {
+    config,
+    styled,
+    css,
+    getCssText,
+    globalCss,
+    keyframes,
+    theme,
+    createTheme,
+} = createStitches({
+    theme: {
+        colors: {
+            rocketseat: '#8257e6'
+        }
+    }    
+})
