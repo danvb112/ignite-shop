b9af186af09ee2880251598828df8657134b3d8b
Daniel Bonasser <danielbonasser@gmail.com>
2023-07-22 12:13:32 -0300

    Create HomeContainer and Product components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
+import { HomeContainer, Product } from '@/styles/pages/home';
 import { styled } from '../styles/index';
+import Image from 'next/image';
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  },
-});
+import camiseta1 from '../assets/camisetas/camisa1.png';
+import camiseta2 from '../assets/camisetas/camisa2.png';
 
 export default function Home() {
   return (
-    <Button>
-      <span>Test</span>
-      Enviar
-    </Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt='' width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta1} alt='' width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
