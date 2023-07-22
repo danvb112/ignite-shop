f4846453ac225e8f2f7304d2c4bd4f4cb50e47e9
Daniel Bonasser <danielbonasser@gmail.com>
2023-07-22 12:32:11 -0300

    Add carousel to product list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
 import { HomeContainer, Product } from '@/styles/pages/home';
-import { styled } from '../styles/index';
+import {useKeenSlider} from 'keen-slider/react';
 import Image from 'next/image';
 
 import camiseta1 from '../assets/camisetas/camisa1.png';
 import camiseta2 from '../assets/camisetas/camisa2.png';
+import camiseta3 from '../assets/camisetas/camisa3.png';
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product className='keen-slider__slide'>
         <Image src={camiseta1} alt='' width={520} height={480} />
 
         <footer>
@@ -17,7 +25,25 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta2} alt='' width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} alt='' width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
         <Image src={camiseta1} alt='' width={520} height={480} />
 
         <footer>
