771ee8e31d7422296f559cf5362ea6134c73f6dc
Daniel Bonasser <danielbonasser@gmail.com>
2023-08-16 22:15:46 -0300

    Create styled components to product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
 import { useRouter } from "next/router"
 
 export default function Product() {
     const {query} = useRouter();
 
     return (
-      <h1>Product: {JSON.stringify(query)}</h1>
+      <ProductContainer>
+        <ImageContainer>
+
+        </ImageContainer>
+
+        <ProductDetails>
+          <h1>Camiseta X</h1>
+          <span>R$ 79,90</span>
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, omnis distinctio? Esse possimus, placeat accusamus minima soluta temporibus nam distinctio quidem voluptate perferendis officiis doloremque illo quo rem nulla doloribus.</p>
+
+          <button>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
     )
   }
